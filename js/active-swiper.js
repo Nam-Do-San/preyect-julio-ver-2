24924b2a38fed6aec6b7c6ec45a982eb2aaf1fc5
nam-do-sam <nrsn2may@gmail.com>
2023-03-08 18:53:40 -0500

    martes-08 test web subiendo al hostgator

--- js/active-swiper.js
+++ js/active-swiper.js
@@ -1,4 +1,7 @@
-var swiper = new Swiper(".mySwiper", {
+
+// procedimientos de cirugías
+
+var swiper = new Swiper(".swi-casos", {
     slidesPerView: 1,
     spaceBetween: 10,
     pagination: {
@@ -19,4 +22,27 @@ var swiper = new Swiper(".mySwiper", {
             spaceBetween: 50,
         },
     },
+});
+
+var swiper = new Swiper(".swi-resultados", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    // breakpoints: {
+    //     640: {
+    //         slidesPerView: 1,
+    //         spaceBetween: 20,
+    //     },
+    //     768: {
+    //         slidesPerView: 1,
+    //         spaceBetween: 40,
+    //     },
+    //     1024: {
+    //         slidesPerView: 1,
+    //         spaceBetween: 50,
+    //     },
+    // },
 });
